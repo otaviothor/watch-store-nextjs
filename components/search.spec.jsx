62a25d396f3e53a1cb3f557b9e4330e422dd070a
Otávio Silva <otaviosilva2632@gmail.com>
2024-09-17 21:11:04 -0300

    feat: testing search component

--- components/search.spec.jsx
+++ components/search.spec.jsx
@@ -1,12 +1,20 @@
 import Search from "./search";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+const doSearch = jest.fn();
 
 describe("Search component", () => {
-  it("should render Search component", () => {
-    render(<Search />);
+  it("should render a form", () => {
+    render(<Search doSearch={doSearch} />);
+    expect(screen.getByRole("form")).toBeInTheDocument();
+  });
 
-    screen.debug();
+  it("should call props.doSearch() when form is submmited", async () => {
+    render(<Search doSearch={doSearch} />);
+    const form = screen.getByRole("form");
 
-    expect(screen.getByTestId("search")).toBeInTheDocument();
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
   });
 });
