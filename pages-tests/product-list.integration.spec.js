c2d078ffbecaa80222faa783734623e9ce1bc38b
Otávio Silva <otaviosilva2632@gmail.com>
2024-10-28 21:25:44 -0300

    feat: create index integration tests, pt2

--- pages-tests/product-list.integration.spec.js
+++ pages-tests/product-list.integration.spec.js
@@ -1,10 +1,28 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import ProductList from "../pages";
 
-describe("ProductList", () => {
-  it("should render ProductList", () => {
-    render(<ProductList />);
+const renderProductList = () => {
+  render(<ProductList />);
+};
 
-    expect(screen.getByTestId("product-list")).toBeInTheDocument();
+describe("ProductList", () => {
+  it("should render ProductList", async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId("product-list")).toBeInTheDocument();
+    });
   });
+
+  fit("should render the ProductCard component 10 times", () => {
+    renderProductList();
+    expect(screen.getAllByTestId("product-card")).toHaveLength(10);
+  });
+
+  it.todo("should render the no products message");
+  it.todo("should render the Search component");
+  it.todo("should filter the product list when a search is performed");
+  it.todo("should display error message when promise rejects");
+  it.todo("should display the total quantity of products");
+  it.todo("should display product (singular) when there is only 1 product");
 });
