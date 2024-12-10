fbe4955e3fde6c98bda6f8fc133f45050e6ffde3
Otávio Silva <otaviosilva2632@gmail.com>
2024-12-09 21:10:22 -0300

    feat: testing cart store with add action and products state

--- store/cart/cart-store.unit.spec.js
+++ store/cart/cart-store.unit.spec.js
@@ -1,22 +1,57 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { useCartStore } from '.';
+import { makeServer } from '../../miragejs/server';
 
 describe('Cart Store', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should return open equals false on initial state', () => {
     const { result } = renderHook(() => useCartStore());
 
     expect(result.current.state.open).toBe(false);
   });
 
+  it('should return an empty array for products on initial state', () => {
+    const { result } = renderHook(() => useCartStore());
+
+    expect(result.current.state.products).toHaveLength(0);
+    expect(Array.isArray(result.current.state.products)).toBe(true);
+  });
+
+  it('should return an empty array for products on initial state', async () => {
+    const products = server.createList('product', 2);
+    const { result } = renderHook(() => useCartStore());
+    const {
+      actions: { add },
+    } = result.current;
+
+    for (const product of products) {
+      act(() => add(product));
+    }
+
+    expect(result.current.state.products).toHaveLength(2);
+  });
+
   it('should toggle open state', () => {
     const { result } = renderHook(() => useCartStore());
+    const {
+      actions: { toggle },
+    } = result.current;
 
     expect(result.current.state.open).toBe(false);
 
-    act(() => result.current.actions.toggle());
+    act(() => toggle());
     expect(result.current.state.open).toBe(true);
 
-    act(() => result.current.actions.toggle());
+    act(() => toggle());
     expect(result.current.state.open).toBe(false);
   });
 });
