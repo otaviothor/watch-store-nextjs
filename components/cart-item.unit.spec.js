64525d8887dcc491f94802df15c9944f55dd2203
Otávio Silva <otaviosilva2632@gmail.com>
2025-01-06 21:17:33 -0300

    feat: changes to improve coverage, pt1

--- components/cart-item.unit.spec.js
+++ components/cart-item.unit.spec.js
@@ -1,5 +1,11 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import CartItem from './cart-item';
+import userEvent from '@testing-library/user-event';
+import { useCartStore } from '../store/cart';
+import { renderHook } from '@testing-library/react-hooks';
+import { setAutoFreeze } from 'immer';
+
+setAutoFreeze(false);
 
 const product = {
   title: 'Relógio bonito',
@@ -72,5 +78,17 @@ describe('CartItem component', () => {
     await fireEvent.click(buttonDecrease);
     await fireEvent.click(buttonDecrease);
     expect(quantity.textContent).toBe('0');
+  });
+
+  it('should call remove() when remove button is clicked', async () => {
+    const result = renderHook(() => useCartStore()).result;
+    const spy = jest.spyOn(result.current.actions, `remove`);
+
+    renderCartItem();
+    const button = screen.getByRole(`button`, { name: /remove/i });
+    await userEvent.click(button);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(product);
   });
 });
